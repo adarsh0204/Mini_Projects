56ca381ba03ed2e30560823559a3366fbb1b5f4d
Adarsh Verma <112921040+adarsh0204@users.noreply.github.com>
2022-11-25 20:43:23 +0530

    Update Cake_Cutting_Project2.py

--- Cake_Cutting_Project2.py
+++ Cake_Cutting_Project2.py
@@ -1,5 +1,17 @@
 #Project 21:
 #Your task is to find if it is possible to cut the cake in the below mentioned ways for a given value N.
+'''
+Given an integer N and cake which can be cut into pieces, each cut should be straight line going from the
+center of the cake to its border. Also, the angle between any two cuts must be a positive integer. Two pieces are equal
+if their appropriate angles are equal.
+
+The given cake can be cut in following three ways:
+
+# Cut the cake into N equal pieces.
+# Cut the cake into N pieces of any size.
+# Cut the cake into N pieces such that no two of them are equal.
+
+'''
 
 
 cakeangle=eval(input("Enter the Angle of the Cake: "))
@@ -26,6 +38,3 @@ else:
 
 
 #Name:Adarsh Verma
-#Roll No: 64
-#Section: K22EQ
-#Reg. No: 12201441 
